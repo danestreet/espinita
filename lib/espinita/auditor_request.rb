8f2d061664c341484bc48dfef0aded996da64ddb
Dan Macon <dnmacon@gmail.com>
2014-12-07 15:39:29 -0500

    work around ActiveSupport::Duration method_missing for calling ago on an integer

--- lib/espinita/auditor_request.rb
+++ lib/espinita/auditor_request.rb
@@ -1,13 +1,13 @@
-module Espinita::AuditorRequest 
+module Espinita::AuditorRequest
   extend ActiveSupport::Concern
 
-  included do 
+  included do
     before_filter :store_audited_user
   end
 
   def store_audited_user
-    RequestStore.store[:audited_user] = self.send(Espinita.current_user_method) #current_user
-    
+    RequestStore.store[:audited_user] = self.__send__(Espinita.current_user_method) #current_user
+
     RequestStore.store[:audited_ip]   = self.try(:request).try(:remote_ip)
   end
 end
